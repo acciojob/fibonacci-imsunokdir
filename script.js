0ebdd5c1f09baa469f7e0cf28be0da36c9000694
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-15 21:07:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function fibonacci(n) {
   if (n === 2) return 1; 
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
-document.getElementById("fiboForm").addEventListener('submit', function(e){
-	e.preventDefault();
+// document.getElementById("fiboForm").addEventListener('submit', function(e){
+// 	e.preventDefault();
 	// const n = document.getElementById("nInput").value;
 	// const res = document.getElementById("res");
 	// res.textContent = `${fibonacci(n)}`
-})
-
+// })
+module.exports = fibonacci
