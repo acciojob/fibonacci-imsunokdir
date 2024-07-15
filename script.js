9b870ab00eaf8d3e02ddaed3dc021607f936a193
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-15 21:01:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-document.getElementById("fiboForm").addEventListener('submit', function(e){
-  e.preventDefault(); // Prevent default form submission behavior
 
-  // Get the input value from the form
-  const n = document.getElementById("nInput").value;
-
-  // Calculate the Fibonacci number
-  const result = fibonacci(n);
-
-  // Update the result element with the calculated value
-  document.getElementById("res").textContent = result;
-});
 function fibonacci(n) {
 // your code here
   if (n <= 0) return 0;
@@ -17,4 +6,10 @@ function fibonacci(n) {
   if (n === 2) return 1; 
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
+document.getElementById("fiboForm").addEventListener('submit', function(e){
+	e.preventDefault();
+	const n = document.getElementById("nInput").value;
+	const res = document.getElementById("res");
+	res.textContent = `${fibonacci(n)}`
+})
 
