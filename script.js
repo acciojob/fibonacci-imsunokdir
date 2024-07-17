ae2a390712b06914645fa3c796267056a5dcf91c
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-17 16:12:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
-
 function fibnocci(n,memo = {}) { 
     if (n <= 0) return 0;
-    if (n === 1) return 1;
+    if (n === 1) return 0;
+    if (n === 2) return 1;
     if (memo[n]) return memo[n];
 
     memo[n] = fibnocci(n - 1, memo) + fibnocci(n - 2, memo);
-    return memo[n];
-} 
+    return memo[n];   
+}  
 
 // const n = parseInt(prompt("Enter n"));
-// alert(fibnocci(n))  
+// alert(fibnocci(n))
 
 module.exports = fibnocci;
